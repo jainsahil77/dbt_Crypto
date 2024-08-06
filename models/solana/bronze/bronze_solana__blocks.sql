cfb173309cbc475d2a5abf2e3456a99fbd71ebec
Sahil Jain <jainsahil77@gmail.com>
2024-08-06 11:59:06 +0530

    Add files via upload

---
+++ models/solana/bronze/bronze_solana__blocks.sql
@@ -0,0 +1,22 @@
+{{ config (
+    materialized = 'view',
+    tags = ['snowflake', 'solana', 'bronze_solana', 'solana_blocks']
+) }}
+
+SELECT
+    record_id,
+    offset_id AS block_id,
+    block_id AS offset_id,
+    block_timestamp,
+    network,
+    chain_id,
+    tx_count,
+    header,
+    ingested_at
+FROM
+    {{ source(
+        'prod',
+        'solana_blocks'
+    ) }}
+WHERE
+    offset_id >= 110547628
---
+++ models/solana/silver/silver_solana__blocks.sql
@@ -0,0 +1,38 @@
+{{ config(
+  materialized = 'incremental',
+  unique_key = "block_id",
+  incremental_strategy = 'delete+insert',
+  cluster_by = ['block_timestamp::DATE'],
+  persist_docs={"relation": true, "columns": true}, 
+  tags = ['snowflake', 'solana', 'silver_solana', 'solana_blocks']
+) }}
+
+WITH base_tables AS (
+  SELECT  
+      *
+  FROM 
+    {{ ref('bronze_solana__blocks') }}
+
+{% if is_incremental() %}
+     WHERE ingested_at >= getdate() - interval '2 days'
+{% endif %}
+)
+
+SELECT
+    block_id :: INTEGER AS block_id,  
+    block_timestamp :: TIMESTAMP AS block_timestamp, 
+    network :: STRING AS network, 
+    chain_id :: STRING AS blockchain, 
+    tx_count :: INTEGER AS tx_count,
+    header :blockHeight :: INTEGER AS block_height, 
+    header :blockTime :: INTEGER AS block_time, 
+    header :blockhash :: VARCHAR AS block_hash, 
+    header :parentSlot :: INTEGER AS previous_block_id, 
+    header :previousBlockhash :: VARCHAR AS previous_block_hash,  
+    ingested_at :: TIMESTAMP AS ingested_at
+FROM 
+   base_tables 
+
+ qualify(ROW_NUMBER() over(PARTITION BY block_id
+  ORDER BY
+    ingested_at DESC)) = 1
